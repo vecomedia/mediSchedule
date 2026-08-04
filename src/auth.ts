6c86cc45b7847a052d06c6612e93fcc64cbb9066
vecomedia <vcolleret@gmail.com>
2026-08-04 18:11:02 +0200

    authBasePath config change

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,8 @@
+import type { NextAuthOptions } from "next-auth";
+import { getServerSession } from "next-auth";
+
 import NextAuth from "next-auth";
+
 import Credentials from "next-auth/providers/credentials";
 
 import { loginSchema } from "@/lib/validations/auth";
